7ecc918db9b495fb701b43a3c4b07f260b8273f2
unknown <aswathbabu2000@gmail.com>
2023-11-12 13:37:09 +0530

    returns String

--- src/Strings/alternativeStringMerge.java
+++ src/Strings/alternativeStringMerge.java
@@ -26,12 +26,54 @@ public class alternativeStringMerge {
         }
     }
 
+    public static String returnMergedString(String str1, String str2) {
+
+        int m = str1.length();
+        int n = str2.length();
+
+        char[] arr = new char[m + n];
+
+        int ptr1 = 0;
+        int ptr2 = 0;
+
+        int index = 0;
+
+        while (ptr1 < m && ptr2 < n) {
+
+            arr[index] = str1.charAt(ptr1);
+            index++;
+            arr[index] = str2.charAt(ptr2);
+
+            ptr1++;
+            ptr2++;
+            index++;
+        }
+
+        while (ptr1 < m) {
+            arr[index] = str1.charAt(ptr1);
+
+            ptr1++;
+            index++;
+        }
+
+        while ((ptr2 < n)) {
+            arr[index] = str2.charAt(ptr2);
+
+            ptr2++;
+            index++;
+        }
+
+        String mergedString = new String(arr);
+        return mergedString;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String str1 = sc.next();
         String str2 = sc.next();
 
-        mergeStrings(str1, str2);
+//        mergeStrings(str1, str2);
+        System.out.println(returnMergedString(str1,str2));
     }
 }
