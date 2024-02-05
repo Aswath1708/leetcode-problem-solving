b16443ff53412104cd62de4a8fbf3d138eb50d45
unknown <aswathbabu2000@gmail.com>
2024-02-06 01:13:21 +0530

    check if string are close

--- src/HashMap/UniqueNumberOfOccurrences.java
+++ src/HashMap/UniqueNumberOfOccurrences.java
@@ -16,13 +16,11 @@ public class UniqueNumberOfOccurences {
     }
 
     public static boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap();
 
         for (int el : arr) map.put(el, map.getOrDefault(el, 0) + 1);
 
-        Set<Integer> set = new HashSet<>();
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) set.add(entry.getValue());
+        Set<Integer> set = new HashSet(map.values());
 
         return map.size() == set.size();
     }
